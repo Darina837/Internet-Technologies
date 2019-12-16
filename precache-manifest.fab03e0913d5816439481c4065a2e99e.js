c406589e4584469a46aea0cd60739772a6492ffd
Darina837 <daryna.koloda@nure.ua>
2019-12-17 00:32:28 +0200

    Updates

---
+++ precache-manifest.fab03e0913d5816439481c4065a2e99e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6442d29fd6fcac4f13bc979b6ab5205",
+    "url": "/Internet-Technologies/index.html"
+  },
+  {
+    "revision": "33636fd5be8564220013",
+    "url": "/Internet-Technologies/static/css/main.272439fc.chunk.css"
+  },
+  {
+    "revision": "1650603b12281df7f7b9",
+    "url": "/Internet-Technologies/static/js/2.788437a9.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Internet-Technologies/static/js/2.788437a9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "33636fd5be8564220013",
+    "url": "/Internet-Technologies/static/js/main.011c47db.chunk.js"
+  },
+  {
+    "revision": "e1316c6a5603b0eda5d0",
+    "url": "/Internet-Technologies/static/js/runtime-main.fba2b60a.js"
+  },
+  {
+    "revision": "8b5d4ae3c4b646ba167ad7fcd75ac806",
+    "url": "/Internet-Technologies/static/media/bg.8b5d4ae3.png"
+  },
+  {
+    "revision": "1d77156d067afd80ee2aac9ab420e76d",
+    "url": "/Internet-Technologies/static/media/img.1d77156d.png"
+  },
+  {
+    "revision": "42a49cef5055c04ba157919f11f7f71a",
+    "url": "/Internet-Technologies/static/media/variants1.42a49cef.png"
+  }
+]);
